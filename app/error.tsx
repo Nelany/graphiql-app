7f70e599071d2d6af667a1f2ac6341f47c864837
Nelany <svetasweetka@gmail.com>
2024-08-18 21:35:33 +0200

    feat: add error and not-found files

---
+++ app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function GlobalError() {
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+      <h3>Reload the application please!</h3>
+    </div>
+  );
+}
