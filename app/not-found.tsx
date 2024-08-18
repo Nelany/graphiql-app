c13b0539225afb27497e035267035eb07938d764
Nelany <svetasweetka@gmail.com>
2024-08-18 21:35:33 +0200

    feat: add error and not-found files

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Ooops... Page not found!</h1>
+      <Link href="/">
+        <button>Go to Main</button>
+      </Link>
+    </div>
+  );
+}
