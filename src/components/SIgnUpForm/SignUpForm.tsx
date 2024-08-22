6d53dfd1cc01d782eec62648bc10173849f091da
Oksana Bondareva <oksanchik252@gmail.com>
2024-08-23 00:01:04 +0300

    feat: add validation

--- src/components/SIgnUpForm/SignUpForm.tsx
+++ src/components/SIgnUpForm/SignUpForm.tsx
@@ -15,7 +15,7 @@ export default function SignUpForm() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<SignUpFormData>();
+  } = useForm<SignUpFormData>({ mode: 'onChange' });
 
   const onSubmit: SubmitHandler<SignUpFormData> = async (data) => {
     const { name, email, password } = data;
@@ -27,22 +27,41 @@ export default function SignUpForm() {
       <div className={styles['input-wrapper']}>
         <label>Enter your name:</label>
         <input className={styles.input} {...register('name', { required: true })} placeholder="Name" />
-        {errors.email && <span>Email is required</span>}
+        {errors.name && <span className={styles['error-validation']}>Name is required</span>}
       </div>
       <div className={styles['input-wrapper']}>
         <label>Enter your email:</label>
-        <input className={styles.input} {...register('email', { required: true })} placeholder="Email" />
-        {errors.email && <span>Email is required</span>}
+        <input
+          className={styles.input}
+          {...register('email', {
+            required: 'Please, enter your email',
+            pattern: {
+              value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              message: 'Enter your email in the format example@email.com',
+            },
+          })}
+          placeholder="Email"
+          type="text"
+        />
+        {errors.email && <span className={styles['error-validation']}>{errors.email.message}</span>}
       </div>
       <div className={styles['input-wrapper']}>
         <label>Enter your password:</label>
         <input
           className={styles.input}
-          {...register('password', { required: true })}
-          type="password"
+          {...register('password', {
+            required: 'Please, enter your password',
+            pattern: {
+              value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+              message:
+                'Password must be at least 8 characters long, contain one letter, one digit, and one special character',
+            },
+          })}
           placeholder="Password"
+          autoComplete="off"
+          type="password"
         />
-        {errors.password && <span>Password is required</span>}
+        {errors.password && <span className={styles['error-validation']}>{errors.password.message}</span>}
       </div>
       <button type="submit">Sign up</button>
     </form>
--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -17,7 +17,7 @@ export default function SignInForm() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<SignInFormData>();
+  } = useForm<SignInFormData>({ mode: 'onChange' });
 
   const onSubmit: SubmitHandler<SignInFormData> = async (data) => {
     const { email, password } = data;
@@ -28,18 +28,37 @@ export default function SignInForm() {
     <form onSubmit={handleSubmit(onSubmit)} className={styles['sign-in-form']}>
       <div className={styles['input-wrapper']}>
         <label>Enter your email:</label>
-        <input className={styles.input} {...register('email', { required: true })} placeholder="Email" />
-        {errors.email && <span>Email is required</span>}
+        <input
+          className={styles.input}
+          {...register('email', {
+            required: 'Please, enter your email',
+            pattern: {
+              value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              message: 'Enter your email in the format example@email.com',
+            },
+          })}
+          placeholder="Email"
+          type="text"
+        />
+        {errors.email && <span className={styles['error-validation']}>{errors.email.message}</span>}
       </div>
       <div className={styles['input-wrapper']}>
         <label>Enter your password:</label>
         <input
           className={styles.input}
-          {...register('password', { required: true })}
-          type="password"
+          {...register('password', {
+            required: 'Please, enter your password',
+            pattern: {
+              value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+              message:
+                'Password must be at least 8 characters long, contain one letter, one digit, and one special character',
+            },
+          })}
           placeholder="Password"
+          autoComplete="off"
+          type="password"
         />
-        {errors.password && <span>Password is required</span>}
+        {errors.password && <span className={styles['error-validation']}>{errors.password.message}</span>}
       </div>
       <button type="submit">Sign in</button>
     </form>
