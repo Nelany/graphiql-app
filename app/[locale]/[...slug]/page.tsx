1437934b23fe4cd1f623feb7260d4f61c0a85d38
Anton Ramarchuk <87061061+RemAntof@users.noreply.github.com>
2024-08-26 08:28:24 +0200

    fix: fix path to Restfull in slug

--- app/[locale]/[...slug]/page.tsx
+++ app/[locale]/[...slug]/page.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import initTranslations from '../../i18n';
 import styles from './page.module.css';
 import { decode64 } from '../../../src/utils/base64';
-import RestFull from '../../../src/components/Clients/RESTfull/RestFull';
+import RestFull from '../../../src/components/Clients/RESTfull/RESTFull';
 import { redirect } from 'next/navigation';
 
 type Props = {
