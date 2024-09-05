91eae74dca096fc3f34ef7604b76032e80a1df60
Oksana Bondareva <oksanchik252@gmail.com>
2024-09-06 02:28:18 +0300

    feat: add sdl reques

--- src/Types/Types.ts
+++ src/Types/Types.ts
@@ -1,4 +1,14 @@
+import { GraphQLType } from 'graphql';
+import { Maybe } from 'graphql/jsutils/Maybe';
+
 export interface KeyValue {
   key: string;
   value: string;
+}
+
+export interface Item {
+  name: string;
+  description?: string | Maybe<string>;
+  type: GraphQLType;
+  parent?: Item;
 }
--- src/components/Clients/GraphQL/GraphQl.tsx
+++ src/components/Clients/GraphQL/GraphQl.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import { encode64 } from '@/utils/base64';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import styles from './GraphQl.module.css';
 import ClientEndpoint from '../RestQlClient/ClientEndpoint/ClientEndpoint';
 import JsonEditor from '../RestQlClient/ClientJsonEditor/JsonEditor';
+import { encode64 } from '@/utils/base64';
 import ResponseStatus from '../RestQlClient/ClientResponse/ResponseStatus/ResponseStatus';
 import GraphQLDocs from '../RestQlClient/GraphQLDocs/GraphQLDocs';
 import Image from 'next/image';
 import ClientEndpointSdl from '../RestQlClient/ClientEndpointSdl/ClientEndpointSdl';
 import KeyValueInputs from '../RestQlClient/KeyValueInputs/KeyValueInputs';
-import styles from './GraphQl.module.css';
+import { buildClientSchema, getIntrospectionQuery, GraphQLSchema } from 'graphql';
+import { toast } from 'react-toastify';
 
 interface Header {
   key: string;
@@ -31,21 +33,18 @@ export default function GraphQL({ endpoint, headers, body, locale }: RestFullPro
   const [requestHeaders, setRequestHeaders] = useState<Header[]>(headers || []);
   const [requestBody, setRequestBody] = useState(body);
   const [endpointUrlSdl, setEndpointUrlSdl] = useState(endpoint ? `${endpoint}?sdl` : '');
+  const [schema, setSchema] = useState<GraphQLSchema | null>(null);
 
   const prepareHeadersParams = (headersArray: Header[]) => {
     return headersArray.map((val) => Object.values(val)).filter((val) => val[0]);
   };
 
-  const [isDocsVisible, setIsDocsVisible] = useState(false);
-
-  const toggleDocsVisibility = () => {
-    setIsDocsVisible(!isDocsVisible);
-  };
-
   useEffect(() => {
     if (!endpointUrl) {
       setEndpointUrlSdl('');
-    } else setEndpointUrlSdl(`${endpoint}?sdl`);
+    } else {
+      setEndpointUrlSdl(`${endpointUrl}?sdl`);
+    }
   }, [endpointUrl]);
 
   useEffect(() => {
@@ -65,6 +64,25 @@ export default function GraphQL({ endpoint, headers, body, locale }: RestFullPro
     }
   }, [endpointUrl, requestHeaders, requestBody, locale]);
 
+  const fetchSDL = async () => {
+    const response = await fetch(endpointUrlSdl, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: getIntrospectionQuery() }),
+    });
+    const result = await response.json();
+    const schemaObject = result.data;
+    const schema = buildClientSchema(schemaObject);
+    setSchema(schema);
+    if (schema instanceof Error) {
+      toast.error(schema.message);
+      return;
+    }
+  };
+
   return (
     <div className={styles.resfullWrapper}>
       <div className={styles.resfullDocsWrapper}>
@@ -74,20 +92,21 @@ export default function GraphQL({ endpoint, headers, body, locale }: RestFullPro
           alt="list-icon"
           width={60}
           height={60}
-          onClick={toggleDocsVisibility}
         />
-        {isDocsVisible && <GraphQLDocs />}
+        {schema && <GraphQLDocs schema={schema} />}
       </div>
       <div className={styles.resfullContainer}>
         <div className={styles.editFieldContainer}>
           <div className={styles.methodEndContainer}>
             <ClientEndpoint value={endpointUrl} onChange={setEndpointUrl} />
-            <button className={styles.buttonSend}>Send</button>
+            <button className={styles.buttonSend}>{t('RESTGraphQL:send')}</button>
           </div>
           <KeyValueInputs value={requestHeaders} onChange={setRequestHeaders} />
           <div className={styles.methodEndContainer}>
             <ClientEndpointSdl value={endpointUrlSdl} onChange={setEndpointUrlSdl} />
-            <button className={styles.buttonSend}>Send</button>
+            <button className={styles.buttonSend} onClick={fetchSDL}>
+              {t('RESTGraphQL:send')}
+            </button>
           </div>
           <JsonEditor value={requestBody} onChange={setRequestBody} />
         </div>
